6ce0940ab49998632eb7ec94345e5222e27cf417
sagaradhikari <sagradhkr48@gmail.com>
2024-08-22 18:23:41 +0545

    wip

---
+++ StudentManagement.Client/Repository/ICountryRepository.cs
@@ -0,0 +1,17 @@
+﻿using StudentManagement_Shared.Models;
+
+namespace StudentManagement.Client.Repository;
+
+public class ICountryRepository
+{
+    Task<Country> AddCountryAsync(Country country);
+    
+    Task<Country> UpdateCountryAsync(Country country);
+
+    Task<Country> DeleteCountryAsync(int countryId);
+
+    Task<List<Country>> GetAllCountriesAsync();
+
+    Task<Country> GetCountryByIdAsync(int countryId);
+    
+}
